bf6923502de23471a81200d2f478cc0bc03c747b
azimxxm <azimxm@gmail.com>
2021-10-22 17:53:33 +0500

    CRUD

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django.forms import *
+from .models import *
+
+class orderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        widgets = {
+            "customer":Select(attrs={
+                'class':'form-control',
+            }),
+            "product":Select(attrs={
+                'class':'form-control',
+            }),
+            "status":Select(attrs={
+                'class':'form-control',
+            }),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
-
+from .forms import *
 
 def home(request):
     orders = Order.objects.all()
@@ -31,5 +31,50 @@ def products(request):
     return render(request, 'accounts/products.html', contaxt)
 
 
-def customer(request):
-    return render(request, 'accounts/customers.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    contaxt = {
+        'customer': customer,
+        'orders':orders,
+        'order_count':order_count
+    }
+    return render(request, 'accounts/customers.html', contaxt)
+
+
+def createOrder(request):
+    form = orderForm()
+    if request.method == "POST":
+        form = orderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    contaxt = {
+        'form':form
+    }
+    return render(request, 'accounts/order_form.html', contaxt)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = orderForm(instance=order)
+
+    if request.method == "POST":
+        form = orderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    contaxt = {
+        'form':form
+    }
+    return render(request, 'accounts/order_form.html', contaxt)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('home')
+    contaxt = {
+        'order':order
+    }
+    return render(request, 'accounts/delete.html', contaxt)
