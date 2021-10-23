358f117e9cff1bf42b893c20426d1b3905357cd6
azimxxm <azimxm@gmail.com>
2021-10-23 15:02:59 +0500

    Adding WSGI setting

--- CRM/wsgi.py
+++ CRM/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
+from my_project import MyWSGIApp
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')
 
 application = get_wsgi_application()
+
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
